cf9c6d77400288eb6dc8127f3e895a93bc37e2b9
Praveen Bhati <127674992+Praveen9719@users.noreply.github.com>
2025-09-20 19:35:56 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api", // Adjust the backend URL 
+  baseURL: import.meta.env.MODE === "development" ? "https://chats-backend-uue7.onrender.com" : "/api", // Adjust the backend URL 
   withCredentials: true,
 });
+
+
+//"http://localhost:5001/api"
